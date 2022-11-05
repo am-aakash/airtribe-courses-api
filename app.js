d4437900cbe209874e87a1f0c1d1431f09596905
Aakash Chaudhary <akashchaudhary12121@gmail.com>
2022-11-05 22:28:16 +0530

    Postgres DB added and Express app init

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const app = express();
+
+// home route
+app.use("/", (req, res) => {
+  res.send("Airtribe courses API working");
+});
+
+// db sequelize
+const db = require("./models/index");
+// db.sequelize.sync({
+//     force: true,
+// });
+
+const CONFIG = require("./config/config");
+
+// Server
+const port = CONFIG.port;
+app.listen(port, () => {
+  console.log(`Listening on: http://localhost:${port}`);
+});
---
+++ config/config.js
@@ -0,0 +1,17 @@
+const CONFIG = {}; // Make this global to use all over the application
+
+CONFIG.app = "development"; //production or development
+CONFIG.port = "3033";
+
+if (CONFIG.app === "production") {
+  // TODO
+} else if (CONFIG.app === "development") {
+  CONFIG.db_dialect = "postgres";
+  CONFIG.db_host = "3.8.164.107";
+  CONFIG.db_port = "5432";
+  CONFIG.db_name = "airtribe";
+  CONFIG.db_user = "postgres";
+  CONFIG.db_password = "9334";
+}
+
+module.exports = CONFIG;
