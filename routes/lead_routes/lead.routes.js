064f713203fcb10906c7917bdcd5e1af7243b3a9
Aakash Chaudhary <akashchaudhary12121@gmail.com>
2022-11-06 16:53:03 +0530

    Search Lead API

--- routes/lead_routes/lead.routes.js
+++ routes/lead_routes/lead.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 
 const lead_registration_controller = require("../../controllers/lead_controllers/lead_registration.controller");
 const update_status_controller = require("../../controllers/lead_controllers/update_status.controller");
+const search_lead_controller = require("../../controllers/lead_controllers/search_lead.controller");
 
 router.post(
   "/lead-registration",
@@ -10,5 +11,7 @@ router.post(
 );
 
 router.post("/update-status", update_status_controller.UpdateStatus);
+
+router.post("/search-lead", search_lead_controller.SearchLead);
 
 module.exports = router;
