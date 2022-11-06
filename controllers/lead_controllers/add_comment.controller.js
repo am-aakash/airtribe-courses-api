e014d84c8022aa5d3fd93603364d7f32fb9dc71c
Aakash Chaudhary <akashchaudhary12121@gmail.com>
2022-11-06 16:56:56 +0530

    Add comment API

---
+++ controllers/lead_controllers/add_comment.controller.js
@@ -0,0 +1,52 @@
+const { v4: uuidv4 } = require("uuid");
+const db = require("../../models");
+const Lead = db.leads;
+const response = require("../../helpers/response.helper");
+
+exports.AddComment = async (req, res) => {
+  let lead_id = req.body.lead_id;
+  let comment = req.body.comment;
+  console.log(req.body);
+  try {
+    let lead = await Lead.findOne({
+      where: {
+        id: lead_id,
+      },
+    });
+    if (!lead) {
+      return response.responseHelper(
+        res,
+        false,
+        "Wrong Lead id",
+        "Unexpected Failure"
+      );
+    }
+    let updatedLead = await lead.update({
+        comment: comment,
+    });
+    if (!updatedLead) {
+      return response.responseHelper(
+        res,
+        false,
+        "Status not updated",
+        "Unexpected Failure"
+      );
+    }
+    return response.responseHelper(
+      res,
+      true,
+      {
+        updatedLead: updatedLead,
+      },
+      "Successfully Added Comment"
+    );
+  } catch (error) {
+    console.log(error);
+    return response.responseHelper(
+      res,
+      false,
+      "Something went wrong.",
+      "Unexpected Failure"
+    );
+  }
+};
--- routes/lead_routes/lead.routes.js
+++ routes/lead_routes/lead.routes.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const lead_registration_controller = require("../../controllers/lead_controllers/lead_registration.controller");
 const update_status_controller = require("../../controllers/lead_controllers/update_status.controller");
 const search_lead_controller = require("../../controllers/lead_controllers/search_lead.controller");
+const add_comment_controller = require("../../controllers/lead_controllers/add_comment.controller");
 
 router.post(
   "/lead-registration",
@@ -13,5 +14,7 @@ router.post(
 router.post("/update-status", update_status_controller.UpdateStatus);
 
 router.post("/search-lead", search_lead_controller.SearchLead);
+
+router.post("/add-comment", add_comment_controller.AddComment);
 
 module.exports = router;
